832fa2e47d3cd18b59eeeeeb949589df0b8941c7
zadiv <nnguionza@gmail.com>
2022-04-23 00:23:44 -0700

    definition: define todo and role model

--- graph/model/role.model.go
+++ graph/model/role.model.go
@@ -1,13 +1,30 @@
 package model
 
+import (
+	"encoding/json"
+	"time"
+)
+
 type Role struct {
-	ID          string  `json:"id"`
-	Name        string  `json:"name"`
-	Description *string `json:"description"`
-	CreatedAt   *string `json:"createdAt"`
-	UpdatedAt   *string `json:"updatedAt"`
-	Slug        *string `json:"slug"`
+	ID          string     `json:"id" rethinkdb:"id,omitempty" redis:"id,omitempty"`
+	Name        string     `json:"name" rethinkdb:"name" redis:"name"`
+	Description *string    `json:"description" rethinkdb:"description" redis:"description"`
+	CreatedAt   *time.Time `json:"created_at" rethinkdb:"created_at"`
+	UpdatedAt   *time.Time `json:"updated_at" rethinkdb:"updated_at"`
+	Slug        *string    `json:"slug" rethinkdb:"slug"`
 }
 
 func (Role) IsSearchResult() {}
 func (Role) IsBaseModel()    {}
+
+func (r *Role) String() string {
+	return r.Name
+}
+
+func (r *Role) UnmarshalBinary(data []byte) error {
+	return json.Unmarshal(data, r)
+}
+
+func (r *Role) MarshalBinary() ([]byte, error) {
+	return json.Marshal(r)
+}
--- graph/model/todo.model.go
+++ graph/model/todo.model.go
@@ -1,20 +1,33 @@
 package model
 
+import (
+	"encoding/json"
+	"time"
+)
+
 type Todo struct {
-	ID          string   `json:"id"`
-	Text        string   `json:"text"`
-	Done        bool     `json:"done"`
-	User        *User    `json:"user"`
-	UserID      string   `json:"userID"`
-	Assigned    []*User  `json:"assigned"`
-	AssignedIDs []string `json:"assignedIDs"`
-	Description *string  `json:"description"`
-	Start       string   `json:"start"`
-	End         *string  `json:"end"`
-	CreatedAt   *string  `json:"createdAt"`
-	UpdatedAt   *string  `json:"updatedAt"`
-	Slug        *string  `json:"slug"`
+	ID          string     `json:"id" rethinkdb:"id,omitempty" redis:"id,omitempty"`
+	Description *string    `json:"description" rethinkdb:"description" redis:"description"`
+	Text        string     `json:"text" rethinkdb:"text" redis:"text"`
+	Done        bool       `json:"done" rethinkdb:"done" redis:"done"`
+	User        *User      `json:"user" rethinkdb:"user" redis:"user"`
+	UserID      string     `json:"user_id" rethinkdb:"user_id,omitempty" redis:"user_id,omitempty"`
+	Assigned    []*User    `json:"assigned" rethinkdb:"assigned" redis:"assigned"`
+	AssignedIDs []string   `json:"assigned_ids" rethinkdb:"assigned_ids,omitempty" redis:"assigned_ids,omitempty"`
+	CreatedAt   *time.Time `json:"created_at" rethinkdb:"created_at"`
+	UpdatedAt   *time.Time `json:"updated_at" rethinkdb:"updated_at"`
+	Slug        *string    `json:"slug" rethinkdb:"slug,omitempty"`
+	Start       time.Time  `json:"start" rethinkdb:"start"`
+	End         *time.Time `json:"end" rethinkdb:"end"`
 }
 
 func (Todo) IsSearchResult() {}
 func (Todo) IsBaseModel()    {}
+
+func (t *Todo) UnmarshalBinary(data []byte) error {
+	return json.Unmarshal(data, t)
+}
+
+func (t *Todo) MarshalBinary() ([]byte, error) {
+	return json.Marshal(t)
+}
